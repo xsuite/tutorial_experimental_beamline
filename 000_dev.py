544d1f8054af1f33f12659c984b849e7f46c18ee
giadarol <giovanni.iadarola@cern.ch>
2024-11-21 09:52:31 +0100

    Status

--- 000_dev.py
+++ 000_dev.py
@@ -1,6 +1,7 @@
 # Try to reproduce this one
 # https://gitlab.cern.ch/acc-models/acc-models-ea/-/blob/en-ea-le/H6/scenarios/positive-120gev-fm-focus/focus.py?ref_type=heads
 from cpymad.madx import Madx
+import numpy as np
 
 mad = Madx()
 mad.input('''
@@ -44,24 +45,40 @@ tw['x_1mrad'] = tw_1mrad.x
 tw['x_1percent'] = tw_1percent.x
 tw.plot('x_1mm x_1mrad x_1percent')
 
-# Add marker for focus point
-s_focus = 500.
-
+# Add markers for focus points
+s_focus = np.linspace(490., 540., 9)
 line.discard_tracker()
-line.insert_element(element=xt.Marker(), name='focus', at_s=s_focus)
+for ii, ss in enumerate(s_focus):
+    line.insert_element(element=xt.Marker(), name=f'ff_{ii+1}', at_s=ss)
 
+# Match waist at focus points
 opt = line.match(
     solve=False,
     betx=10, bety=10, alfx=0, alfy=0,
-    vary=xt.VaryList(['kq10', 'kq11', 'kq12', 'kq13', 'kq14', 'kq15', 'kq16']),
+    vary=xt.VaryList(['kq10', 'kq11', 'kq12', 'kq13', 'kq14', 'kq15', 'kq16'], step=1e-3),
     targets=[
-        xt.TargetSet(at='focus', betx=xt.LessThan(0.5), bety=xt.LessThan(0.5),
-                                 alfx=0, alfy=0, dx=0, dy=0),
+        xt.TargetSet(at='ff_5', betx=xt.LessThan(15), bety=xt.LessThan(12),
+                                alfx=0, alfy=0, dx=0, dy=0),
         xt.TargetSet(at='xced.x0410440', betx=xt.LessThan(750), bety=xt.LessThan(750),
                                          alfx=0, alfy=0),
         xt.TargetSet(at='xcsv.x0410384', betx=xt.LessThan(40), bety=xt.LessThan(40),
                                          alfx=0),
     ]
 )
+opt.step(20)
+opt.targets['ff_5_dx'].weight = 10000
+opt.targets['ff_5_dy'].weight = 10000
+opt.step(20)
+
+import tfs
+df = tfs.read('../acc-models-ea/H6/scenarios/positive-120gev-fm-focus/focus_ff_5.tfs')
+
+# line['kq10'] = -68.64
+# line['kq11'] = 159.97
+# line['kq12'] = 43.29
+# line['kq13'] = -105.95
+# line['kq14'] = 155.96
+# line['kq15'] = 166.85
+# line['kq16'] = -124.16
 
 
